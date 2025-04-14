68af21cf70f0b63f575f73f2a710bbe41a71e139
shashanks-06 <surjekarshashank@gmail.com>
2025-04-14 18:41:55 +0530

    implemented DriverFactory class

--- src/main/java/Shashank/WebAutomation/com/qa/factory/DriverFactory.java
+++ src/main/java/Shashank/WebAutomation/com/qa/factory/DriverFactory.java
@@ -1,4 +1,49 @@
 package Shashank.WebAutomation.com.qa.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class DriverFactory {
+
+    public WebDriver driver;
+
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    /**
+     * This method is used to initialize the threadlocal driver on the basis of given
+     * browser
+     *
+     * @param browser
+     * @return this will return tldriver.
+     */
+    public WebDriver init_driver(String browser){
+        System.out.println("Browser Value is " + browser);
+
+        if (browser.equals("chrome")){
+            WebDriverManager.chromedriver().setup();
+            tlDriver.set(new ChromeDriver());
+        }else if (browser.equals("edge")){
+            WebDriverManager.edgedriver().setup();
+            tlDriver.set(new EdgeDriver());
+        }else {
+            System.out.println("Please pass the  correct Browser Value : " + browser);
+        }
+
+        getDriver().manage().deleteAllCookies();
+        getDriver().manage().window().maximize();
+        return getDriver();
+    }
+
+
+    /**
+     * this is used to get the driver with ThreadLocal
+     *
+     * @return
+     */
+    public static synchronized WebDriver getDriver(){
+        return tlDriver.get();
+    }
+
 }

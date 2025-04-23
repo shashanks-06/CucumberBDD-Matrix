98dcda2480c569463b9cfc50d6b16561605ec5fe
shashanks-06 <surjekarshashank@gmail.com>
2025-04-23 16:21:01 +0530

    steps of AccountsPage

---
+++ src/test/java/stepdefinitions/AccountsPageSteps.java
@@ -0,0 +1,32 @@
+package stepdefinitions;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AccountsPageSteps {
+    @Given("user has already logged in to the application")
+    public void userHasAlreadyLoggedInToTheApplication(DataTable dataTable) {
+        
+    }
+
+    @Given("user is on the Accounts Page")
+    public void userIsOnTheAccountsPage() {
+        
+    }
+
+    @Given("user is on Accounts page")
+    public void userIsOnAccountsPage() {
+        
+    }
+
+    @Then("user gets accounts section")
+    public void userGetsAccountsSection(DataTable dataTable) {
+        
+    }
+
+    @And("accounts section count should be {int}")
+    public void accountsSectionCountShouldBe(int count) {
+    }
+}

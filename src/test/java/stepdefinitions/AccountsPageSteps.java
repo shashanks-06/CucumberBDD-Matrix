4e58341af69d629d3b3350a1f224cb96b4c551b2
shashanks-06 <surjekarshashank@gmail.com>
2025-04-26 13:04:29 +0530

    Steps of AccountsPage done

--- src/test/java/stepdefinitions/AccountsPageSteps.java
+++ src/test/java/stepdefinitions/AccountsPageSteps.java
@@ -7,6 +7,7 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 import java.util.List;
 import java.util.Map;
@@ -17,9 +18,9 @@ public class AccountsPageSteps {
     private AccountsPage accountsPage;
 
     @Given("user has already logged in to the application")
-    public void userHasAlreadyLoggedInToTheApplication(DataTable dataTable) {
+    public void userHasAlreadyLoggedInToTheApplication(DataTable credTable) {
 
-        List<Map<String, String>> credsList =  dataTable.asMaps();
+        List<Map<String, String>> credsList =  credTable.asMaps();
 
         String username = credsList.get(0).get("username");
         String password = credsList.get(0).get("password");
@@ -33,20 +34,26 @@ public class AccountsPageSteps {
 
     @Given("user is on the Accounts Page")
     public void userIsOnTheAccountsPage() {
-        
+        String title = accountsPage.getAccountsPageTitle();
+        System.out.println("Accounts Page Title : " + title);
     }
 
-    @Given("user is on Accounts page")
-    public void userIsOnAccountsPage() {
-        
-    }
 
     @Then("user gets accounts section")
-    public void userGetsAccountsSection(DataTable dataTable) {
-        
+    public void userGetsAccountsSection(DataTable sectionsTable) {
+
+        List<String> expected_AccountsSectionList = sectionsTable.asList();
+        System.out.println("Expected Accounts Section List : " + expected_AccountsSectionList);
+
+        List<String> actual_AccountsSectionList = accountsPage.getAccountsSectionList();
+        System.out.println("Actual Accounts Section List : " + actual_AccountsSectionList);
+
+        Assert.assertTrue(expected_AccountsSectionList.containsAll(actual_AccountsSectionList));
+
     }
 
     @And("accounts section count should be {int}")
-    public void accountsSectionCountShouldBe(int count) {
+    public void accountsSectionCountShouldBe(int expectedSectionCount) {
+        Assert.assertEquals(accountsPage.getAccountsSectionCount(), expectedSectionCount);
     }
 }
